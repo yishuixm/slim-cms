10c0317fed867aba0fd65166d0be9320cf2b2bdc
yishuixm <1830802211@qq.com>
2016-09-24 11:29:47 +0800

    更新验证

--- class/yishuixm/middleware/RouteInit.php
+++ class/yishuixm/middleware/RouteInit.php
@@ -17,6 +17,42 @@ class RouteInit
         $this->ci = $ci;
     }
 
+    private function isLogined(){
+        $db = $this->ci->get("db");
+        $session = $this->ci->get("session");
+        $username = $session->username;
+        $user_id = $session->user_id;
+        $login_info = $session->login_info;
+
+        if(!$login_info){
+            return false;
+        }
+
+        return $db->has("user", [
+            "AND"           => [
+                "login_info[=]"     => $login_info,
+                "username[=]"       => $username,
+                "id[=]"             => $user_id,
+            ]
+        ]);
+    }
+
+    private function isAccessible($path){
+        $db = $this->ci->get("db");
+        $session = $this->ci->get("session");
+        $user_id = $session->user_id;
+
+        $user_accessible = $db->get("user_accessible", "accessible", [
+            "uid[=]"            => $user_id
+        ]);
+        $user_accessible = unserialize($user_accessible);
+        $accessible_id = $db->get("accessible", "id", [
+            "path[=]"            => $path
+        ]);
+
+        return in_array($accessible_id, $user_accessible);
+    }
+
     /**
      *
      *
@@ -31,37 +67,31 @@ class RouteInit
         $Config = new Config($this->ci->get('db'), 'config', true);
 
         $configs = $Config->select([],["app","name","value"]);
-        foreach ($configs['result'] as $config){
+        foreach ($configs['result'] as $config) {
             $config_list["{$config['app']}.{$config['name']}"] = $config['value'];
         }
-        var_dump($config_list);
 
 
-
-
-        $config = $this->ci->get('config');
         $session = $this->ci->get('session');
         $router = $this->ci->get('router');
         $uri = $request->getUri();
         $path = $uri->getPath();
 
-        $purview = $config->get('purview');
 
-        $no_login = true;
+        $login_url = unserialize($config_list["COMMON.LOGIN_URL"]);
 
 
-        foreach ($purview['no-login'] as $nl_url){
-            if(preg_match($nl_url['match'], $path)){
-                $no_login = false;
-                $login_url = $router->pathFor($nl_url['login'], [], []);
-                break;
+        foreach ($login_url as $url){
+            if(!preg_match($url, $path)){
+                continue;
+            }
+            if(!$this->isLogined()){
+                return $response->withRedirect('/login', 303);
+            }
+            if(!$this->isAccessible($path)){
+                $response->withRedirect('/pessimists', 303);
             }
         }
-
-        if(!$no_login){
-            return $response->withRedirect($login_url,303);
-        }
-
 
         return $next($request, $response);
     }
